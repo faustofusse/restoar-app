9a90108184b3684b5e63081bf851a8cd49a818d8
luciorebora <lrebora@tekgenesis.com>
2019-04-30 23:36:29 -0300

    Agrego React Navigation al proyecto, queda pendiente migrar la navegacion a otro paquete para que quede mas prolijo

--- index.js
+++ index.js
@@ -4,12 +4,33 @@ import configureStore from './src/configureStore';
 import React from 'react';
 import Mozo from './src/components/mozo/Mozo';
 import {name as appName} from './app.json';
+import { createStackNavigator, createAppContainer } from 'react-navigation';
+const RootStack = createStackNavigator(
+  {
+      Mozo: Mozo
+  },
+  {
+      initialRouteName: 'Mozo',
+      defaultNavigationOptions: {
+          headerStyle:{
+            backgroundColor: '#b71c1c',
+            height: 70,
+          },
+          headerTintColor : '#fff',
+          headerTitleStyle:{
+              textAlign: 'center',
+              flex: 1
+          }
+      }
+  }
+);
 
+const AppContainer = createAppContainer(RootStack);
 const store = configureStore();
 
 const ReactNativeRedux = () => (
     <Provider store={store}>
-        <Mozo />
+        <AppContainer />
     </Provider>
 );
 
